df4e60a46e6e9ea023ed01ff9d04141de27c9ac0
DefffinitiveOx72 <166840943+DefffinitiveOx72@users.noreply.github.com>
2025-06-05 20:40:57 +0300

    1

--- source/scripts/index.js
+++
@@ -1,8 +0,0 @@
-const navMain = document.querySelector('.main-header__nav');
-const openButton = document.querySelector('.main-header__toggle');
-
-navMain.classList.remove('main-header__nav--nojs');
-
-openButton.onclick = function() {
-  navMain.classList.toggle('main-header__nav--opened');
-};
